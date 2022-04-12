4693e62710de1c63cb4d4fec38a2282abaecab63
stunfish <aftclement@gmail.com>
2022-04-12 21:46:17 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 import sys
 
+
 lst = []
-#importer l'image
 img = pygame.image.load('assets/car.png')
+img2 = pygame.image.load('assets/car2.png')
+ing3 = pygame.image.load('assets/balle.png.png')
 
 a = 0
 while a < 1170:
@@ -39,10 +41,16 @@ class Jeu:
         self.grille = Grille(self.ecran)
 
     def fonction_principale(self):
-        #vitesse voiture
-        step = 1
-        p1 = 10
-        p2 = 10
+
+        step = 4
+
+        p1 = 950
+        p2 = 830
+        p3 = 30
+        p4 = 830
+        p5 = 370
+        p6 = 700
+
         while self.jeu_encours:
 
             for event in pygame.event.get():
@@ -52,24 +60,55 @@ class Jeu:
 
             self.ecran.fill((200, 200, 200))
             self.grille.afficher()
-            #bouger la voiture
             key_input = pygame.key.get_pressed()
-            if key_input[pygame.K_LEFT]:
+            if key_input[pygame.K_LEFT] and p1 >= 0:
                 p1 -= step
-            if key_input[pygame.K_UP]:
+            if key_input[pygame.K_UP] and p2 >= -50:
                 p2 -= step
-            if key_input[pygame.K_RIGHT]:
+            if key_input[pygame.K_RIGHT] and p1 <= 980:
                 p1 += step
-            if key_input[pygame.K_DOWN]:
+            if key_input[pygame.K_DOWN] and p2 <= 830:
                 p2 += step
-            #afficher image
-            self.ecran.blit(img, (p1, p2))
+            #voiture 2 mouvement
+            if key_input[pygame.K_q] and p3 >= 0:
+                p3 -= step
+            if key_input[pygame.K_z] and p4 >= -50:
+                p4 -= step
+            if (key_input[pygame.K_d]) and p3 <= 980:
+                p3 += step
+            if key_input[pygame.K_s] and p4 <= 830:
+                p4 += step
+
+            self.ecran.blit(img2, (p1, p2))
+            self.ecran.blit(img, (p3, p4))
+            self.ecran.blit(ing3, (p5, p6))
+
 
             pygame.display.update()
             pygame.display.flip()
+
+img = pygame.image.load('assets/car.png')
+
+a = 0
+while a < 1170:
+    a += 30
+    lst.append(((a, 0), (a, 1200)))
+lst2 = []
+
+a = 0
+while a < 1170:
+    a += 30
+    lst2.append(((0, a), (1200, a)))
+lst3 = (lst + lst2)
+
 
 
 if __name__ == '__main__':
     pygame.init()
     Jeu().fonction_principale()
     pygame.quit()
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,32 +1,105 @@
 import pygame
 import sys
-from pygame.locals import*
-img = pygame.image.load('car.jpg')
-pygame.init()
-fps=30
-fpsclock=pygame.time.Clock()
-sur_obj=pygame.display.set_mode((600,600))
-pygame.display.set_caption("Keyboard_Input")
-White=(255,255,255)
-p1=10
-p2=10
-step=15
-while True:
-    sur_obj.fill(White)
 
-    for eve in pygame.event.get():
-        if eve.type==pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    key_input = pygame.key.get_pressed()
-    if key_input[pygame.K_LEFT]:
-        p1 -= step
-    if key_input[pygame.K_UP]:
-        p2 -= step
-    if key_input[pygame.K_RIGHT]:
-        p1 += step
-    if key_input[pygame.K_DOWN]:
-        p2 += step
-    sur_obj.blit(img, (p1, p2))
-    pygame.display.update()
-    fpsclock.tick(fps)
+lst = []
+img = pygame.image.load('assets/car.png')
+img2 = pygame.image.load('assets/car2.png')
+
+
+a = 0
+while a < 1170:
+    a += 30
+    lst.append(((a, 0), (a, 1200)))
+lst2 = []
+
+a = 0
+while a < 1170:
+    a += 30
+    lst2.append(((0, a), (1200, a)))
+lst3 = (lst + lst2)
+
+
+class Grille:
+
+    def __init__(self, ecran):
+        self.ecran = ecran
+        self.lignes = lst3
+
+    def afficher(self):
+        for ligne in self.lignes:
+            pygame.draw.line(self.ecran, (100, 100, 100), ligne[0], ligne[1], 2)
+
+
+class Jeu:
+
+    def __init__(self):
+
+        self.ecran = pygame.display.set_mode((1200, 1000))
+        pygame.display.set_caption('ex.1')
+        self.jeu_encours = True
+        self.grille = Grille(self.ecran)
+
+
+    def fonction_principale(self):
+
+        step = 2
+        p1 = 950
+        p2 = 10
+        p3 = 10
+        p4 = 10
+        while self.jeu_encours:
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.ecran.fill((200, 200, 200))
+            self.grille.afficher()
+            key_input = pygame.key.get_pressed()
+            if key_input[pygame.K_LEFT] and p1 >= 0:
+                p1 -= step
+            if key_input[pygame.K_UP]:
+                p2 -= step
+            if key_input[pygame.K_RIGHT]:
+                p1 += step
+            if key_input[pygame.K_DOWN]:
+                p2 += step
+            #voiture 2 mouvement
+            if key_input[pygame.K_q]:
+                p3 -= step
+            if key_input[pygame.K_z]:
+                p4 -= step
+            if key_input[pygame.K_d]:
+                p3 += step
+            if key_input[pygame.K_s]:
+                p4 += step
+            self.ecran.blit(img2, (p1, p2))
+            self.ecran.blit(img, (p3, p4))
+
+
+
+            pygame.display.update()
+            pygame.display.flip()
+
+
+img = pygame.image.load('assets/car.png')
+
+a = 0
+while a < 1170:
+    a += 30
+    lst.append(((a, 0), (a, 1200)))
+lst2 = []
+
+a = 0
+while a < 1170:
+    a += 30
+    lst2.append(((0, a), (1200, a)))
+lst3 = (lst + lst2)
+
+
+
+if __name__ == '__main__':
+    pygame.init()
+    Jeu().fonction_principale()
+    pygame.quit()
